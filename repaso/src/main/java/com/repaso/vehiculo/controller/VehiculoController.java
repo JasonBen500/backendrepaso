c7d909bfa46e7ba820b71d55a9c1dafae6055359
yeyson <bennyruiz752@gmail.com>
2026-09-18 17:56:58 -0600

    "18/09/2026"

--- repaso/src/main/java/com/repaso/vehiculo/controller/VehiculoController.java
+++ repaso/src/main/java/com/repaso/vehiculo/controller/VehiculoController.java
@@ -9,11 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/vehiculos")
@@ -34,11 +35,11 @@ public class VehiculoController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<MessageResponse> crearVehiculo(VehiculoDTO vehiculoDTO) {
+    public ResponseEntity<MessageResponse> crearVehiculo(@RequestBody VehiculoDTO vehiculoDTO) {
         try {
             vehiculoService.crearVehiculo(vehiculoDTO);
-            return ResponseEntity.ok(new MessageResponse("Vehiculo agregado con exito"));
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new MessageResponse("Vehiculo agregado con exito"));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new MessageResponse("Error al crear el vehiculo"));
@@ -46,12 +47,12 @@ public class VehiculoController {
     }
 
     @PutMapping("/modificar/{idVehiculo}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<MessageResponse> actualizarVehiculo(Integer idVehiculo, VehiculoDTO vehiculoDTO) {
+    public ResponseEntity<MessageResponse> actualizarVehiculo(
+            @PathVariable Integer idVehiculo,
+            @RequestBody VehiculoDTO vehiculoDTO) {
         try {
             vehiculoService.modificarVehiculo(idVehiculo, vehiculoDTO);
-            return ResponseEntity
-                    .ok(new MessageResponse("Vehiculo actualizado con exito"));
+            return ResponseEntity.ok(new MessageResponse("Vehiculo actualizado con exito"));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new MessageResponse("Error al actualizar el vehiculo"));
@@ -59,11 +60,10 @@ public class VehiculoController {
     }
 
     @PutMapping("/anular/{idVehiculo}")
-    public ResponseEntity<MessageResponse> anularVehiculo(Integer idVehiculo) {
+    public ResponseEntity<MessageResponse> anularVehiculo(@PathVariable Integer idVehiculo) {
         try {
             vehiculoService.anularVehiculo(idVehiculo);
-            return ResponseEntity
-                    .ok(new MessageResponse("Vehiculo anulado con exito"));
+            return ResponseEntity.ok(new MessageResponse("Vehiculo anulado con exito"));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new MessageResponse("Error al anular el vehiculo"));
